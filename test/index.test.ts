6fb43498807db33fb38be6e5cb9e10a5d1da7990
makbarsyahwana <makbarsyahwana@gmail.com>
2022-09-01 12:27:16 +0700

    update testing file and jest package

--- test/index.test.ts
+++ test/index.test.ts
@@ -19,3 +19,30 @@ describe("POST /", () => {
         // expect(response.statusCode).toBe(500);
       });
 })
+
+describe("POST /", () => {
+    it("should return 404", async () => {
+        const response = await request(baseUrl).post("/");
+        // console.log(response)
+        expect(response.statusCode).toBe(404);
+        // expect(response.statusCode).toBe(500);
+      });
+})
+
+describe("PUT /", () => {
+    it("should return 404", async () => {
+        const response = await request(baseUrl).put("/");
+        // console.log(response)
+        expect(response.statusCode).toBe(404);
+        // expect(response.statusCode).toBe(500);
+      });
+})
+
+describe("DELETE /", () => {
+    it("should return 404", async () => {
+        const response = await request(baseUrl).delete("/");
+        // console.log(response)
+        expect(response.statusCode).toBe(404);
+        // expect(response.statusCode).toBe(500);
+      });
+})
