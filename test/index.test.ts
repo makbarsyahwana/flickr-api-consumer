f623de6cd8ba534aaafb6e4e89407735f2f4cd68
makbarsyahwana <makbarsyahwana@gmail.com>
2022-09-01 12:20:02 +0700

    add jest testing

---
+++ test/index.test.ts
@@ -0,0 +1,21 @@
+import {describe, expect, it} from '@jest/globals';
+const request = require("supertest")
+const baseUrl = "http://localhost:8000"
+
+describe("GET /", () => {
+    it("should return 200", async () => {
+        const response = await request(baseUrl).get("/");
+        // console.log(response)
+        expect(response.statusCode).toBe(200);
+        // expect(response.statusCode).toBe(500);
+      });
+})
+
+describe("POST /", () => {
+    it("should return 404", async () => {
+        const response = await request(baseUrl).post("/");
+        // console.log(response)
+        expect(response.statusCode).toBe(404);
+        // expect(response.statusCode).toBe(500);
+      });
+})
